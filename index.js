d3eedb348b305355f5b7eb65940f36dd8e09e44b
Jose Mendoza <pball626@gmail.com>
2021-04-07 22:35:56 -0500

    commit tree 5f79e36e0beb7940753a7e2fbd37e2f5f06693dd

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+// our user input get submited and greet takes that input as a string
+function greet(string){
+  //we create a variable that represent that string after its been parsed and turned an integer
+  let time = parseInt(string)
+  // need to take this string and we need to parse it 
+  if (time < 12){
+    return('Good Morning')
+  }
+  else if (time > 17){
+    return('Good Evening')
+  }
+  else{
+    return('Good Afternoon')
+  }
+  // console.log('Goood Afternoon!')
+  // return console.log('Good Morning!')
+  // }
+}
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  document.getElementById('greeting').innerText = string
+}
